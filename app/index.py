a879cb5e03e8ac47bdfd1a59e06286dc1db75236
nando <nando@hex7.com>
2020-06-04 14:28:46 -0400

    footer

--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import os
 
@@ -10,14 +10,14 @@ app = Flask(__name__)
 def home():
     html = get_header()
     for image in get_images():
-        html += '<tr><td>' + image + '<br><img src=' + image + ' width=100%></td></tr>'
-    html += '</table></body></html>'
+        html += '<p>' + image + '<br><img src=' + image + ' width=100%>'
+    html += get_footer()
     return html
 
 
 def get_header():
-    return '<html><head><title>protests and looting</title></head><body><table width=100%>' + \
-           '<h1>Protest and Looting Images</h1>SEDME<p>'
+    return '<html><head><title>protests and looting</title></head><body>' + \
+           '<h1>Protest and Looting Images</h1>SEDME -- Hex7 Internet Solutions<p>'
 
 def get_images():
     images = []
@@ -26,6 +26,19 @@ def get_images():
     for item in r.json()['items']:
         images.append(item['pagemap']['cse_image'][0]['src'])
     return images
+
+
+def get_footer():
+    return '<p><br><p><center><b><font size=5>&copy;2000-2020 </font></b>' + \
+           '<a target=_blank href=http://hex7.com><b><font size=5>Hex 7 Internet Solutions</font></b></a><br>' + \
+           request.headers.get('User-Agent') + '<br>' + get_ip() + '</body></html>'
+
+
+def get_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        return request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        return request.remote_addr
 
 
 if __name__ == '__main__':
